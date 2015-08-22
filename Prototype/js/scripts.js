2a0429a211ba0915953d037cd3c6fb6174cefb43
Maria Szubski <MSzubski89@gmail.com>
2015-08-22 14:52:18 -0400

    Prototype: JavaScript, Timer Canvas
    • Fixed bug with time intervals
    • Arc draws full circle at 1:00 and clears at 0:00
    • Cleaned up timer code and comments

--- Prototype/js/scripts.js
+++ Prototype/js/scripts.js
@@ -1,94 +1,103 @@
-// ------------------------------------- Timer Code Block
+/* ____________________________________ Timer */
 var oneMinuteTimer = (
 	function(){
 
-		// ------------------------------- Start & Reset Functions
+		/* ------------------------------ On Start/Reset */
 		function startTimer() {
-			sec = 59;
-			countdown = setInterval(currentTime, 1000);
-			arcAnim = setInterval(drawRingTimer, 50);
-			toggle = false;
-			toggleBtn.setAttribute('src', 'img/reset.svg');
+			n_sec = 60;
+			a_sec = 0;
+			toggle_startStop = false;
+			toggle_btn.setAttribute('src', 'img/reset.svg');
+
+			drawNum();	// Write n_sec before first interval
+			drawArc();	// Draw arc before first interval
+			arc_interval = setInterval(drawArc, 500); // match arc frame rate
+			num_interval = setInterval(drawNum, 1000);
 		}
 
 		function resetTimer(){
-			clearInterval(countdown);
-			clearInterval(arcAnim);
-			toggle = true;
-			toggleBtn.setAttribute('src', 'img/play.svg');
-			timeValue.innerHTML = "1:00";
-			rSec=0;
-			ringTimer.clearRect(0,0,174,174);
+			clearInterval(arc_interval);
+			clearInterval(num_interval);
+			a_timer.clearRect(0 , 0, 174, 174);	// Reset canvas
+
+			toggle_startStop = true;
+			toggle_btn.setAttribute('src', 'img/play.svg');
+			n_timer.nodeValue = "1:00";
 		}
 
-		// ------------------------------- Countdown Output
-		function currentTime() {
+		/* ------------------------------ Countdown Output */
+		function drawNum() {
 			switch(true){
-				case (sec < 10 && sec > 0):
-					timeValue.innerHTML = 0 + ':0' + sec;
+				case (n_sec < 10 && n_sec > 0):
+					n_timer.nodeValue = 0 + ':0' + n_sec;
 					break;
-				case (sec === 0):
-					timeValue.innerHTML = 0 + ':0' + sec;
-					clearInterval(countdown);
-					clearInterval(arcAnim);
-					rSec=0;
+				case (n_sec === 0):
+					clearInterval(num_interval);
+					clearInterval(arc_interval);
+					a_timer.clearRect(0 , 0, 174, 174); // Reset Canvas
+					n_timer.nodeValue = 0 + ':0' + n_sec;
+					break;
+				case (n_sec === 60):
+					n_timer.nodeValue = '1:00';
 					break;
 				default:
-					timeValue.innerHTML = 0 + ':' + sec;
+					n_timer.nodeValue = 0 + ':' + n_sec;
 					break;
 			}
-			sec--;
+			n_sec--;
 		}
 
-		// ------------------------------- Ring timer
-		function drawRingTimer(){
+		/* ------------------------------ Arc Timer */
+		function drawArc(){
 			// Reset Canvas
-			ringTimer.clearRect(0,0,174,174);
-			// Increment ring length
-			/* length change per frame = [ (radian multiplier) / (60 seconds * frames per second) ] * (elapsed seconds) */
-			length_i = ( 2 / (60*20) ) * rSec;
+			a_timer.clearRect(0 , 0, 174, 174);
 
-			/* count from top of circle) */
-			arcVal = 1.5 + length_i;
+			// Length of arc increment
+			length_i =	(2 / (60 * 2)) * a_sec;
+							/*[ (radian multiplier) / (60 seconds * frames per second) ] * (elapsed intervals) */
 
-			/* calculate radians */
-			endPoint = arcVal * Math.PI;
+			// Start counting from top of circle
+			a_val = 1.5 + length_i;
 
-			// Draw timer ring
-			ringTimer.beginPath();
-			ringTimer.arc (
+			// Convert to radians
+			a_endPoint = a_val * Math.PI;
+
+			// Draw timer arc
+			a_timer.beginPath();
+			a_timer.arc(
 				87,					// x position
 				87,					// y position
 				82,					// radius
-				(1.5 * Math.PI),	// start
-				endPoint,			// end
+				(3.5 * Math.PI),	// start @ top + 2pi
+				a_endPoint,			// end
 				true					// counter-clockwise
 			);
-			ringTimer.lineWidth = 10;
-			ringTimer.strokeStyle='teal';
-			ringTimer.stroke();
-			rSec++;
+			a_timer.lineWidth = 10;
+			a_timer.strokeStyle = 'teal';
+			a_timer.stroke();
+			a_sec += 1;
 		}
 
+		/* ------------------------------ Variables */
+		var 	n_sec,		// Number countdown value
+				n_interval;	// Number interval
 
+		var 	a_sec,		// Arc countdown value
+				a_interval,	// Arc interval
+				length_i,	// Arc increment
+				a_val,		// Arc increment, top of circle
+				a_endPoint;	// Arc increment, in radians
 
-		// ------------------------------- Variables
-		var countdown, sec;
-		var toggle = true;
+		var 	toggle_startStop = true; // Start/Reset countdown on click
+		var 	toggle_btn = document.getElementById('toggle_btn');
 
-		var rSec=0;
-		var length_i, endPoint, arcVal;
+		var 	timer_container = document.getElementById('timer_container');
+		var 	n_timer = document.getElementById('n_timer').firstChild;
+		var 	a_timer = document.getElementById('a_timer').getContext('2d');
 
-
-		var timer = document.getElementById('timeValue');
-		var startReset = document.getElementById('startReset');
-		var toggleBtn = document.getElementById('toggleBtn');
-		var ringTimer = document.getElementById('ringTimer').getContext('2d');
-		var ringTrack = new Image();
-
-		// ------------------------------- Start/Reset Events
-		startReset.onclick = function(){
-			switch(toggle){
+		/* ------------------------------ Start/Reset on Click */
+		timer_container.onclick = function(){
+			switch(toggle_startStop){
 				case true:
 					startTimer();
 					break;
@@ -97,20 +106,18 @@ var oneMinuteTimer = (
 					break;
 			}
 		};
-
 	}()
 );
 
+/* ____________________________________ New Chord Overlay */
 var overlayContainer = document.getElementById('addChordOverlay');
 var newChordCombo = document.getElementById('newChordCombo');
 var submitChordCombo = document.getElementById('submitChordCombo');
 
 submitChordCombo.onclick = function(){
-	console.log('click');
 	overlayContainer.className += " hidden ";
 };
 
 newChordCombo.onclick = function(){
-	console.log('click');
 	overlayContainer.className = "overlayContainer";
 };
